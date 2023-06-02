1dee8e5418701394a0d617cddbeeed5d72ba1ea7
thekingvice <villarrealvicente92@gmail.com>
2023-06-02 12:11:07 -0700

    fixed

---
+++ src/Accordian/Accordian.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function Accordian() {
+  const [accordionTabs, setAccordionTabs] = useState([
+    { id: 1, isOpen: false },
+    { id: 2, isOpen: false },
+    { id: 3, isOpen: false },
+  ]);
+
+  function handleOpenTab(num: number) {
+    const array = [...accordionTabs];
+    function closeAllTabs() {
+      for (let i = 0; i < array.length; i++) {
+        array[i].isOpen = false;
+      }
+    }
+    if (array[num].isOpen) {
+      closeAllTabs();
+    } else {
+      closeAllTabs();
+      array[num].isOpen = true;
+    }
+    setAccordionTabs(array);
+  }
+
+  return (
+    <ul className="Accordian__wrapper">
+      {accordionTabs.map((tab, index) => (
+        <>
+          <li
+            key={tab.id}
+            onClick={() => {
+              handleOpenTab(index);
+            }}
+          >{`Tab ${tab.id}`}</li>
+          <li style={{ display: tab.isOpen ? "block" : "none" }}>
+            {tab.id}. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Sit amet purus gravida quis blandit turpis cursus. Leo duis ut diam
+            quam nulla porttitor massa id.
+          </li>
+        </>
+      ))}
+    </ul>
+  );
+}
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./Counter/Counter";
+import Carrousel from "./Carrousel/Carrousel";
+import Accordian from "./Accordian/Accordian";
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <Carrousel />
+      <Accordian />
+    </>
+  );
+}
+
+export default App;
