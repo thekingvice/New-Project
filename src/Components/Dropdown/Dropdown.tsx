49d899b0d41e4dd511cb13135a9041ff7b4ce507
thekingvice <villarrealvicente92@gmail.com>
2023-06-06 23:03:12 -0700

    commit

--- src/Components/Dropdown/Dropdown.tsx
+++ src/Components/Dropdown/Dropdown.tsx
@@ -1,15 +1,35 @@
 import "./Dropdown.css";
+import Gist from "super-react-gist";
 
 export default function Dropdown() {
   return (
-    <div className="Dropdown__wrapper">
-      Hover me
-      <ul className="Dropdown__options">
-        <li className="Dropdown__option">Option 1</li>
-        <li className="Dropdown__option">Option 2</li>
-        <li className="Dropdown__option">Option 3</li>
-        <li className="Dropdown__option">Option 4</li>
-      </ul>
+    <div className="Dropdown">
+      <h1 className="Dropdown__h1">Dropdown</h1>
+      <div className="Dropdown__wrapper">
+        Hover me
+        <ul className="Dropdown__options">
+          <li className="Dropdown__option">Option 1</li>
+          <li className="Dropdown__option">Option 2</li>
+          <li className="Dropdown__option">Option 3</li>
+          <li className="Dropdown__option">Option 4</li>
+        </ul>
+      </div>
+      <div className="Dropdown__gist">
+        <Gist url="https://gist.github.com/thekingvice/f6b611dd407e9e6ac82e768c892dff13" />
+      </div>
     </div>
   );
 }
+
+// .Dropdown__wrapper {
+//     position: relative;
+// }
+
+// .Dropdown__wrapper:hover > .Dropdown__options {
+//     display: block;
+// }
+
+// .Dropdown__options {
+//     position: absolute;
+//     display: none;
+// }
