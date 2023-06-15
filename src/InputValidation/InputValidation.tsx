fe2ecc2fb1f24e40836190ca846dd0f6052c6599
thekingvice <villarrealvicente92@gmail.com>
2023-06-14 19:21:36 -0700

    commit

--- src/InputValidation/InputValidation.tsx
+++ src/InputValidation/InputValidation.tsx
@@ -1,14 +1,60 @@
+import "./InputValidation.css";
 import { useState } from "react";
+import Gist from "super-react-gist";
 
 export default function InputValidation() {
   const [inputValue, setInputValue] = useState("");
 
-  function handleSubmit() {}
+  const [result, setResult] = useState("");
+
+  const [textColor, setTextColor] = useState(true);
+
+  function handleValidation(str: string) {
+    const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    const isMatch = regex.test(str);
+    if (isMatch) {
+      setResult("Valid Input");
+      setTextColor(true);
+    } else {
+      setResult("Error: Invalid Input");
+      setTextColor(false);
+    }
+  }
 
   return (
-    <div>
-      <input type="time" name="input" id="" />
-      <button></button>
+    <div className="InputValidation">
+      <h1 className="InputValidation__h1">Input Validation</h1>
+      <div className="InputValidation__wrapper">
+        <input
+          className="InputValidation__input"
+          type="text"
+          name="input"
+          id="email"
+          placeholder="Enter Email Address"
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+            console.log(inputValue);
+          }}
+        />
+        <button
+          className="InputValidation__submit"
+          onClick={() => {
+            handleValidation(inputValue);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+      <p
+        className="InputValidation__result"
+        style={{ background: textColor ? "green" : "red" }}
+      >
+        {result}
+      </p>
+      <div className="InputValidation__gist">
+        <Gist url="https://gist.github.com/thekingvice/dcc75008e68207ae4a659fca0fb025e7" />
+      </div>
     </div>
   );
 }
