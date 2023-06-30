f6c923ce00bccdeb25cf0170a75bee79c01785d8
thekingvice <villarrealvicente92@gmail.com>
2023-06-29 23:24:27 -0700

    100

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import "animate.css/animate.compat.css";
 import "animate.css";
+import { Link, Element } from "react-scroll";
 import ScrollAnimation from "react-animate-on-scroll";
 import Counter from "./Components/Counter/Counter";
 import Carrousel from "./Components/Carrousel/Carrousel";
@@ -32,22 +33,108 @@ function App() {
             onClick={toggleMenu}
             src="/hamburger_menu_images/hamburger-menu.svg"
             alt="menu"
+            style={{ visibility: isOpen ? "hidden" : "visible" }}
           />
-          <div className="Navigation__menu">
+          <div
+            className="Navigation__menu"
+            style={{ visibility: isOpen ? "visible" : "hidden" }}
+          >
             <img
               src="/hamburger_menu_images/close.svg"
               alt=""
               className="Navigation__close-icon"
+              onClick={toggleMenu}
             />
-            <a href="" className="Navigation__option" id="section1">
-              Section 1
-            </a>
-            <a href="" className="Navigation__option" id="section2">
-              Section 2
-            </a>
-            <a href="" className="Navigation__option" id="section3">
-              Section 3
-            </a>
+            <Link
+              className="Link"
+              to="section1"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Counter
+            </Link>
+            <Link
+              className="Link"
+              to="section2"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Progress Bar
+            </Link>
+            <Link
+              className="Link"
+              to="section3"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Accordian
+            </Link>
+            <Link
+              className="Link"
+              to="section4"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Tool Tip
+            </Link>
+            <Link
+              className="Link"
+              to="section5"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Pop Up
+            </Link>
+            <Link
+              className="Link"
+              to="section6"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Dropdown
+            </Link>
+            <Link
+              className="Link"
+              to="section7"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Tabs
+            </Link>
+            <Link
+              className="Link"
+              to="section8"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Radio Buttons
+            </Link>
+            <Link
+              className="Link"
+              to="section10"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Toggle Switch
+            </Link>
+            <Link
+              className="Link"
+              to="section11"
+              smooth={true}
+              duration={500}
+              offset={-65}
+            >
+              Input Validation
+            </Link>
           </div>
         </div>
       </nav>
@@ -64,44 +151,77 @@ function App() {
       </section>
 
       {/* animate__animated animate__shakeY */}
+      <Element name="section1">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <Counter />
+        </ScrollAnimation>
+      </Element>
 
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <Counter />
-      </ScrollAnimation>
+      <Element name="section2">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <ProgressBar />
+        </ScrollAnimation>
+      </Element>
 
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <ProgressBar />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <Carrousel />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <Accordian />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <ToolTip />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <Popup />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <Dropdown />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <Tabs />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <RadioButtons />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <ToggleSwitch />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <InputValidation />
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
-        <HamburgerMenu />
-      </ScrollAnimation>
+      <Element name="section3">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <Carrousel />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section4">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <Accordian />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section5">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <ToolTip />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section6">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <Popup />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section7">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <Dropdown />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section8">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <Tabs />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section9">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <RadioButtons />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section10">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <ToggleSwitch />
+        </ScrollAnimation>
+      </Element>
+
+      <Element name="section11">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <InputValidation />
+        </ScrollAnimation>
+      </Element>
+
+      {/* <Element name="section12">
+        <ScrollAnimation animateIn="fadeIn" offset={100} animateOnce={true}>
+          <HamburgerMenu />
+        </ScrollAnimation>
+      </Element> */}
     </div>
   );
 }
