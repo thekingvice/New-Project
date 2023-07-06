623286e69d28c5c5abc1d2d9ef28be3002eba4d2
thekingvice <villarrealvicente92@gmail.com>
2023-07-05 17:40:54 -0700

    added social links

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -23,115 +23,152 @@ export default function Navigation() {
           className="Navigation__menu"
           style={{
             width: isOpen ? "14rem" : "0rem",
-            padding: isOpen ? "0.5rem 1rem" : "0rem",
+            padding: isOpen ? "0.5rem 1rem 2rem 0.5rem" : "0rem",
             opacity: isOpen ? "100%" : "0%",
           }}
         >
-          <img
-            src="/hamburger_menu_images/close.svg"
-            alt=""
-            className="Navigation__close-icon"
-            onClick={toggleMenu}
-          />
-          <Link
-            className="Link"
-            to="section1"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Counter
-          </Link>
-          <Link
-            className="Link"
-            to="section2"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Progress Bar
-          </Link>
-          <Link
-            className="Link"
-            to="section3"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Image Carrousel
-          </Link>
-          <Link
-            className="Link"
-            to="section4"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Accordian
-          </Link>
-          <Link
-            className="Link"
-            to="section5"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Tool Tip
-          </Link>
-          <Link
-            className="Link"
-            to="section6"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Pop Up
-          </Link>
-          <Link
-            className="Link"
-            to="section7"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Dropdown
-          </Link>
-          <Link
-            className="Link"
-            to="section8"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Tabs
-          </Link>
-          <Link
-            className="Link"
-            to="section9"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Radio Buttons
-          </Link>
-          <Link
-            className="Link"
-            to="section10"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Toggle Switch
-          </Link>
-          <Link
-            className="Link"
-            to="section11"
-            smooth={true}
-            duration={500}
-            offset={-50}
-          >
-            Input Validation
-          </Link>
+          <div className="Navigation__menu-options">
+            <img
+              src="/hamburger_menu_images/close.svg"
+              alt=""
+              className="Navigation__close-icon"
+              onClick={toggleMenu}
+            />
+            <Link
+              className="Link"
+              to="section1"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Counter
+            </Link>
+            <Link
+              className="Link"
+              to="section2"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Progress Bar
+            </Link>
+            <Link
+              className="Link"
+              to="section3"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Image Carrousel
+            </Link>
+            <Link
+              className="Link"
+              to="section4"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Accordian
+            </Link>
+            <Link
+              className="Link"
+              to="section5"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Tool Tip
+            </Link>
+            <Link
+              className="Link"
+              to="section6"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Pop Up
+            </Link>
+            <Link
+              className="Link"
+              to="section7"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Dropdown
+            </Link>
+            <Link
+              className="Link"
+              to="section8"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Tabs
+            </Link>
+            <Link
+              className="Link"
+              to="section9"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Radio Buttons
+            </Link>
+            <Link
+              className="Link"
+              to="section10"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Toggle Switch
+            </Link>
+            <Link
+              className="Link"
+              to="section11"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              Input Validation
+            </Link>
+          </div>
+          <div className="Navigation__social-wrapper">
+            <a
+              target="_blank"
+              href="https://github.com/thekingvice"
+              className="Navigation__social-link"
+            >
+              <img
+                className="Navigation__social-icon"
+                src="/github.svg"
+                alt="github"
+              />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/vicente-villarreal-44b804266/"
+              className="Navigation__social-link"
+            >
+              <img
+                className="Navigation__social-icon"
+                src="/linkedin.svg"
+                alt="linkedin"
+              />
+            </a>
+            <a
+              target="_blank"
+              href="https://my-website-pi-three.vercel.app/"
+              className="Navigation__social-link"
+            >
+              <img
+                className="Navigation__social-icon"
+                src="/my-site.svg"
+                alt="portfolio-site"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </nav>
